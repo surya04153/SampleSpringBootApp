a920b37d6f046856841bbbdb58e0289a09b669eb
ashu <ashudubey91@gmail.com>
2019-02-18 09:51:50 +0100

    uploaded springboot project

---
+++ src/main/java/com/ashu/Greetings/HelloworldApplication.java
@@ -0,0 +1,23 @@
+package com.ashu.Greetings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class HelloworldApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "<marquee behavior=\"scroll\" direction=\"left\" scrollamount=\"10\"><h1>Hello World...</h1></marquee>\n" + "";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
+
